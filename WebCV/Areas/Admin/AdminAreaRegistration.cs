fc0a32c2d6c1effcbf6461b9776080dcf02146ae
duongdg <nguyenduonghd96@gmail.com>
2018-04-15 16:05:50 +0700

    add layout home

--- WebCV/Areas/Admin/AdminAreaRegistration.cs
+++ WebCV/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace WebCV.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", controller = "Home", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "WebCV.Areas.Admin.Controllers" }
             );
         }
     }
